c943e5b191571036c9050dbaa965faeaaaab70e5
Ross Schlaikjer <ross@schlaikjer.net>
2017-03-08 18:59:17 -0500

    Refactor activity to package

--- app/src/main/java/com/schlaikjer/findbugs/activity/MainActivity.java
+++ app/src/main/java/com/schlaikjer/findbugs/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.schlaikjer.findbugs;
+package com.schlaikjer.findbugs.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.schlaikjer.findbugs.R;
 
 public class MainActivity extends AppCompatActivity {
 
