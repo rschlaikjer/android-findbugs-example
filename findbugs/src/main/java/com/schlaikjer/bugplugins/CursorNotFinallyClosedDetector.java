c08a77b91f0cbfb6b14bcdeef25b0ebcc22ae93b
Ross Schlaikjer <ross@schlaikjer.net>
2017-03-08 22:29:46 -0500

    Simple printing of relevant methods

--- findbugs/src/main/java/com/schlaikjer/bugplugins/CursorNotFinallyClosedDetector.java
+++ findbugs/src/main/java/com/schlaikjer/bugplugins/CursorNotFinallyClosedDetector.java
@@ -1,10 +1,16 @@
 package com.schlaikjer.bugplugins;
 
+import org.apache.bcel.classfile.LocalVariable;
+import org.apache.bcel.classfile.LocalVariableTable;
+import org.apache.bcel.classfile.Method;
+
 import edu.umd.cs.findbugs.BugAccumulator;
 import edu.umd.cs.findbugs.BugReporter;
-import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
 
-public class CursorNotFinallyClosedDetector extends OpcodeStackDetector {
+public class CursorNotFinallyClosedDetector extends BytecodeScanningDetector {
+
+    private static final String ANDROID_CURSOR = "Landroid/database/Cursor;";
 
     protected final BugAccumulator bugAccumulator;
 
@@ -13,8 +19,21 @@ public class CursorNotFinallyClosedDetector extends OpcodeStackDetector {
     }
 
     @Override
-    public void sawOpcode(int seen) {
+    public void visitMethod(Method method) {
+        super.visitMethod(method);
+        LocalVariableTable localVariableTable = method.getLocalVariableTable();
+        if (variableTableContainsType(localVariableTable, ANDROID_CURSOR)) {
+            System.out.println(method.getCode());
+        }
+    }
 
+    private static boolean variableTableContainsType(LocalVariableTable table, String type) {
+        for (LocalVariable variable : table.getLocalVariableTable()) {
+            if (type.equals(variable.getSignature())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
